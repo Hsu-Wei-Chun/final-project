2e16ab124d477c6ce97e8c1393b696f89b3a6284
Hsu-Wei-Chun <abc0756139@gmail.com>
2025-05-28 09:35:30 +0800

    Add files via upload

---
+++ final project/analyze_input.py
@@ -0,0 +1,83 @@
+# Initialize difficulty_categories as an empty dictionary
+difficulty_categories = {}
+
+difficulty_categories.update({
+
+    "兒少保護": {
+        "keywords": ["虐童", "兒童保護", "孩子被打", "性侵害", "受虐兒", "學校霸凌"],
+        "resources": [
+            ["兒童福利聯盟", "https://www.children.org.tw/"],
+            ["保護專線113", "https://www.113.gov.tw/"],
+            ["張老師基金會（青少年）", "https://www.teacherchang.org.tw/"],
+            ["家庭支持服務網", "https://family.swcb.gov.tw/"],
+            ["勵馨基金會（性別與兒少保護）", "https://www.goh.org.tw/"]
+        ]
+    },
+
+    "法律協助": {
+        "keywords": ["打官司", "法律問題", "無辜", "被告", "欠錢", "調解"],
+        "resources": [
+            ["法律扶助基金會", "https://www.laf.org.tw/"],
+            ["台灣司改會", "https://www.jrf.org.tw/"],
+            ["台北市政府法律諮詢", "https://dosw.gov.taipei/"],
+            ["民間司法改革基金會", "https://www.jrf.org.tw/"],
+            ["法律百科（律師公會）", "https://lawbank.com.tw/"]
+        ]
+    },
+
+    "健康醫療": {
+        "keywords": ["生病", "醫藥費", "看病", "醫療補助", "慢性病", "癌症"],
+        "resources": [
+            ["健保署醫療補助專區", "https://www.nhi.gov.tw/"],
+            ["財團法人癌症希望基金會", "https://www.ecancer.org.tw/"],
+            ["罕見疾病基金會", "https://www.tfrd.org.tw/"],
+            ["病人自主推廣協會", "https://www.adct.org.tw/"],
+            ["醫療補助資源查詢平台", "https://www.helppatient.org/"]
+        ]
+    },
+
+    "長照照護": {
+        "keywords": ["照顧長輩", "失能", "照護", "老人", "長照機構", "照顧壓力"],
+        "resources": [
+            ["長照服務專區（長照2.0）", "https://1966.gov.tw/"],
+            ["弘道老人福利基金會", "https://www.hondao.org.tw/"],
+            ["台灣長期照護專業協會", "https://www.tltcpa.org.tw/"],
+            ["家總（家庭照顧者關懷總會）", "https://www.familycare.org.tw/"],
+            ["長照資源整合網", "https://ltcnet.tw/"]
+        ]
+    },
+
+    "社交困難／孤立": {
+        "keywords": ["沒朋友", "孤單", "被排擠", "社交障礙", "沒人懂我"],
+        "resources": [
+            ["張老師基金會", "https://www.teacherchang.org.tw/"],
+            ["心生活心理諮商", "https://www.heartlife.org.tw/"],
+            ["青平台社會設計", "https://www.cet-taiwan.org/"],
+            ["陽光社會福利基金會（人際重建）", "https://www.sunshine.org.tw/"],
+            ["NPOHub社會連結資源平台", "https://www.npohub.org/"]
+        ]
+    },
+
+    "精神障礙相關支持": {
+        "keywords": ["思覺失調", "躁鬱症", "精神病", "幻覺", "吃藥", "被歧視"],
+        "resources": [
+            ["心路基金會（身心障礙服務）", "https://www.hsinlu.org.tw/"],
+            ["台灣精神健康協會", "https://www.tmha.org.tw/"],
+            ["中華心理衛生協會", "https://www.mhat.org.tw/"],
+            ["衛福部精神病人社區資源", "https://mental.mohw.gov.tw/"],
+            ["自立生活聯盟（精神障礙者）", "https://www.ilru.org.tw/"]
+        ]
+    },
+
+    "急難救助": {
+        "keywords": ["火災", "地震", "天災", "緊急", "突然沒地方住", "危機處理"],
+        "resources": [
+            ["衛福部急難救助", "https://www.mohw.gov.tw/cp-117-492-1.html"],
+            ["紅十字會", "https://www.redcross.org.tw/"],
+            ["伊甸基金會急難服務", "https://www.edens.org.tw/"],
+            ["天主教善牧基金會", "https://www.good-shepherd.org.tw/"],
+            ["地方社會局急難窗口（依縣市）", "https://www.gov.tw/"]
+        ]
+    }
+
+})
